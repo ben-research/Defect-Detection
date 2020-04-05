e32f92d3159abfc00b8c63fd2bb77df214901f7e
Ben Mussay <cyber.is.all.you.need@gmail.com>
2020-04-05 21:54:53 +0300

    update main
    next stage is to combine image processing with auto-encoder

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
-import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
 import argparse
 import cv2
 from pathlib import Path
-
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 __author__ = "Ben Mussay"
 
 
 def main():
     parser = argparse.ArgumentParser(description="Defect-Detection in Chips using Image Restoration")
-    parser.add_argument("--path", type=str, help="Full path to the defected image")
+    parser.add_argument("--path-inspect", type=str, help="Full path to the defected image_3d")
+    parser.add_argument("--path-reference", type=str, help="Full path to the reference image_3d")
     args = parser.parse_args()
 
     print("***** Image Restoration *****")
     print("Written By: {}".format(__author__))
-    print(args.path)
-    image = cv2.imread(str(Path(args.path)))
-    print("Original Image dimensions:")
-    print(np.array(image).shape)
+    image_3d = cv2.imread(str(Path(args.path_inspect)))
+    print("Defected Image dimensions:")
+    print(np.array(image_3d).shape)
 
-    cv2.imshow('Original Image', image)
+    image_3d_reference = cv2.imread(str(Path(args.path_reference)))
+    print("Reference Image dimensions:")
+    print(np.array(image_3d_reference).shape)
+
+    cv2.imshow('Defected Image', image_3d)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    print("Gray-Scale Image dimensions:")
-    print(np.array(image).shape)
-
-    cv2.imshow('Gray Scale Image', image)
+    cv2.imshow('Defected Image', image_3d_reference)
     cv2.waitKey()
+    cv2.destroyAllWindows()
+
+    # convert 3-d image to 1-d image
+    image_2d = cv2.cvtColor(image_3d, cv2.COLOR_BGR2GRAY)
+    image_2d_reference = cv2.cvtColor(image_3d_reference, cv2.COLOR_BGR2GRAY)
 
 
 if __name__ == "__main__":
